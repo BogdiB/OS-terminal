6d6e4260083aa28bd4de283b0c58b42f747bcf9e
BogdiB <107000314+BogdiB@users.noreply.github.com>
2023-01-09 23:14:06 +0200

    arrow key detection

--- terminalOS.cpp
+++ terminalOS.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <cstring>
 #include <unistd.h>
+#include <termios.h>
 #define PATH_MAX 4096
 #define COMM_SIZE 100
 
 char historyFileName[] = "history.txt";
+int historyFileLine;
 
 bool deleteFile(char fileName[])
 {
@@ -41,31 +43,61 @@ bool commandDecrypt()
 {
     char initialCommand[COMM_SIZE], command[COMM_SIZE], c;
     short i = 0, cmdlen;
-    // constructing the command strings
-    while((c = getc(stdin)) != '\n')
+    // checking for arrow keys
+    c = std::cin.peek();
+    if (c != EOF && c == 27)
     {
-        // keep the initial command for keeping history
-        initialCommand[i] = c;
-        // make the command lowercase
-        command[i] = tolower(c);
-        ++i;
-        // this if is for validating the size of the input
-        // (so that the number of characters taken doesn't go over the size limit (COMM_SIZE), and thus, cause an error)
-        if (i >= COMM_SIZE) // leaves room for the \0 character as the last character
+        c = getc(stdin); // special char (0 or 224 ascii)
+        c = getc(stdin); // '[' char
+        c = getc(stdin); // A (65 ascii) for up, B (66 ascii) for down
+        // probably shouldn't delete the cin clear and ignore for reasons I forgot to write down when making this
+        std::cin.clear();
+        std::cin.ignore();
+        if (c == 65)
         {
-            // clearing the stdin
-            std::cin.clear();
-            std::cin.ignore();
-            std::cout << "The maximum number of characters you can input is " << COMM_SIZE - 1 << ".\n";
-            initialCommand[i] = '\0';
-            command[i] = '\0';
-            return 1;
+            // up arrow key
+            std::cout << "up key\n" << '\e'; // erase this after things are done
+            /* int x;
+            std::cin >> x;
+            std::cout << x << '\n'; */
+            // get command[] and put in history
         }
+        else if (c == 66)
+        {
+            // down arrow key
+            std::cout << "down key\n"; // erase this after things are done
+            // get command[] and put in history
+        }
+        return true;
     }
-    cmdlen = i;
-    initialCommand[i] = '\0';
-    addHistory(initialCommand);
-    command[i] = '\0';
+    else
+    {
+        // constructing the command strings
+        while((c = getc(stdin)) != '\n' && c > 0)
+        {
+            // keep the initial command for keeping history
+            initialCommand[i] = c;
+            // make the command lowercase
+            command[i] = tolower(c);
+            ++i;
+            // this if is for validating the size of the input
+            // (so that the number of characters taken doesn't go over the size limit (COMM_SIZE), and thus, cause an error)
+            if (i >= COMM_SIZE) // leaves room for the \0 character as the last character
+            {
+                // clearing the stdin
+                std::cin.clear();
+                std::cin.ignore();
+                std::cout << "The maximum number of characters you can input is " << COMM_SIZE - 1 << ".\n";
+                initialCommand[i] = '\0';
+                command[i] = '\0';
+                return 1;
+            }
+        }
+        cmdlen = i;
+        initialCommand[i] = '\0';
+        addHistory(initialCommand);
+        command[i] = '\0';
+    }
     // TODO: ADD initialCommand to history
     // if command is empty, just continue
     if (cmdlen == 0)
@@ -84,6 +116,7 @@ bool commandDecrypt()
 
 int main(int argc, char **argv)
 {
+
     // this whole part of the code is just for the main messages, pretty much everything important happens in commandDecrypt()
     char currentPath[PATH_MAX];
 
